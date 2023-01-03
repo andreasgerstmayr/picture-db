c97d337fe4f406b19112bee2014c40c75d6a1ef0
Andreas Gerstmayr <andreas@gerstmayr.me>
2023-01-03 20:07:45 +0100

    enable WAL for the sqlite db

--- pkg/util.go
+++ pkg/util.go
@@ -7,7 +7,7 @@ import (
 
 func openDatabase(dbPath string) (*gorm.DB, error) {
 	// create or open sqlite db
-	db, err := gorm.Open(sqlite.Open("file:"+dbPath+"?_foreign_keys=1"), &gorm.Config{
+	db, err := gorm.Open(sqlite.Open("file:"+dbPath+"?_foreign_keys=on&_journal_mode=WAL"), &gorm.Config{
 		//Logger: logger.Default.LogMode(logger.Info),
 	})
 	if err != nil {
